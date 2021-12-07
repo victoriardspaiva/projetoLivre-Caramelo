e2fb7cdb3405cfbf8cf90c4981817d2d9b528410
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2021-12-06 22:59:42 -0300

    get by id ok

--- src/controllers/placesController.js
+++ src/controllers/placesController.js
@@ -17,14 +17,14 @@ const createPlace = async (req, res) => {
     const { name, about, district, email, contact, pix, host, quantity, animal } = req.body
     try {
         const newPlace = new Places({
-            name, 
-            about, 
-            district, 
-            email, 
-            contact, 
-            pix, 
-            host, 
-            quantity, 
+            name,
+            about,
+            district,
+            email,
+            contact,
+            pix,
+            host,
+            quantity,
             animal,
             _id: new mongoose.Types.ObjectId()
         })
@@ -50,11 +50,11 @@ const getBySearch = async (req, res) => {
     try {
         if (id) {
             search = await Places.findById(req.query.id)
+            res.status(200).json(search)
         }
-
-
+        nome != undefined ? search = await Places.find(req.query.nome) : res.status(500).send("Host not found")
     } catch (e) {
-        res.status(400).json({
+        res.status(500).json({
             message: e.message
         })
     }
